4ea70f7627c8aff67ece55eb8af782d5eb1d1ce4
jaguardeer <pdeloz@gmail.com>
2024-02-19 16:32:00 -0600

    find VsInstanceId with vswhere.exe

--- build-run-c-version.ps1
+++ build-run-c-version.ps1
@@ -1,4 +1,5 @@
-Enter-VsDevShell -VsInstanceId 6db2c180 -Arch amd64 -SkipAutomaticLocation
+$vs_id = C:"\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -property instanceId
+Enter-VsDevShell -VsInstanceId $vs_id -Arch amd64 -SkipAutomaticLocation
 cl -I $env:VULKAN_SDK/Include .\enum-vulk.c /link $env:VULKAN_SDK/lib/vulkan-1.lib
 if ($?) {
     ./enum-vulk.exe
